6516db8800749f74911ce42f78c7ce8e8b1cc5fa
IllaNozdrevatih <noz220399@gmail.com>
2021-10-19 20:53:59 +0300

    add - model, light, shadow, settings to OrbitControls

--- src/script.js
+++ src/script.js
@@ -1,8 +1,17 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import * as dat from 'dat.gui'
 
+class BaseModel {
+    constructor( name ) {
+        this.name = name
+        this.scene = null
+    }
+}
+// global variables
+const skullModel = new BaseModel('OSG_Scene');
 /**
  * Base
  */
@@ -14,20 +23,43 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = '#fff'
 
 /**
- * Textures
+ * Models
  */
-const textureLoader = new THREE.TextureLoader()
+const GLTFloader = new GLTFLoader();
 
+/**
+ * Scull Model
+ */
+const addScullModel = ( function (x = 0,y , z = 0){
+    GLTFloader.load( '/gltf/scene.gltf', function ( model ) {
+        model.scene.scale.set(0.1, 0.1, 0.1);
+        model.scene.position.x = x
+        model.scene.position.y = y
+        model.scene.position.z = z
+
+
+        model.scene.traverse( function( node ) {
+            if ( node.isMesh || node.isLight ) node.castShadow = true;
+            if ( node.isMesh || node.isLight ) node.receiveShadow = false;
+        } );
+
+        scene.add(model.scene)
+    }, undefined, function ( error ) {
+        console.error( error );
+    } )
+} );
+addScullModel(0, 0.13)
 /**
  * Objects
  */
-const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xcacaca })
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
 
-const planeGeometry = new  THREE.PlaneGeometry(1,1,5)
+const planeGeometry = new  THREE.PlaneGeometry(0.25,0.25,1)
 const plane = new THREE.Mesh(planeGeometry, planeMaterial)
-// plane.receiveShadow = true;
+plane.receiveShadow = true;
 
 plane.rotation.x = Math.PI * 0.5 * -1;
 
@@ -79,13 +111,15 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0.75, 1)
+camera.position.set(0, 0.4, 0.4)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-
+controls.maxPolarAngle = Math.PI/2;
+controls.minDistance = 0.2;
+controls.maxDistance = 0.7;
 /**
  * Renderer
  */
@@ -94,6 +128,9 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+renderer.outputEncoding = THREE.sRGBEncoding;
 
 /**
  * Animate
